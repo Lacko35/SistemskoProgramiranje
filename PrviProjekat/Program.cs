dc2904ca71860894b8945114a8529ec84f35e8b0
Lacko35 <lazar@gmail.com>
2025-09-29 18:21:10 +0200

    prvi zadatak

---
+++ PrviProjekat/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+//pozivi kojima testiramo funkcionalnost samog Web servera
+//http://localhost:8080/?q=Belgrade&days=3&aqi=yes
+//http://localhost:8080/?q=Paris&days=1&aqi=no
+//http://localhost:8080/?q=NepostojeciGrad
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string url = "http://localhost:8080/";
+
+        WebServerWithThreads server = new WebServerWithThreads(url);
+
+        server.Start();
+
+        Console.WriteLine("Pritisnite Enter za zaustavljanje servera...");
+        Console.ReadLine();
+
+        server.Stop();
+    }
+}
